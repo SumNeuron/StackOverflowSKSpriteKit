a585d9a8ce77fccd3ac97b7e288fc1582e2ba18b
Sumner <sumneuron@gmail.com>
2017-01-24 08:59:50 +0100

    first commit

---
+++ StackOverflow/DifficultyScene.swift
@@ -0,0 +1,9 @@
+//
+//  DifficultyScene.swift
+//  StackOverflow
+//
+//  Created by Sumner on 1/22/17.
+//  Copyright © 2017 Sumner. All rights reserved.
+//
+
+import Foundation
---
+++ StackOverflow/MenuScene.swift
@@ -0,0 +1,9 @@
+//
+//  MenuScene.swift
+//  StackOverflow
+//
+//  Created by Sumner on 1/22/17.
+//  Copyright © 2017 Sumner. All rights reserved.
+//
+
+import Foundation
